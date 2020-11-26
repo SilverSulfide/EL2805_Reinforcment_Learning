53ec96d8542cec430175f816eea27eda75dc9295
andrzejperzanowski <amper@kth.se>
2020-11-26 15:54:45 +0100

    val iter added

--- lab1/main.py
+++ lab1/main.py
@@ -8,26 +8,18 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 maze = ut.Maze()
+"""
 V, policy = ut.dynamic_programming(maze, 20)
-path = maze.simulate((0, 0, 6, 5), policy)
+path = maze.simulate((0, 0, 6, 5), policy, method='DynProg')
 
-
-class AnimateGame:
-    def __init__(self, path):
-        self.maze = ut.Maze()
-        self.path = path
-
-    def animate(self, i):
-        moves = self.path[i]
-        self.maze.board[moves[0]][moves[1]] = 2
-        self.maze.board[moves[2]][moves[3]] = -1
-        self.maze.draw()
-        self.maze.board[moves[0]][moves[1]] = 0
-        self.maze.board[moves[2]][moves[3]] = 0
-
-
-total = AnimateGame(path)
+# init animation
+total = ut.AnimateGame(path)
 fig = plt.figure(1, figsize=(total.maze.board.shape[1], total.maze.board.shape[1]))
 
 anim = animation.FuncAnimation(fig, total.animate, frames=len(path), interval=300)
 anim.save('results/mino.gif', writer='imagemagick')
+"""
+
+# ------------- Value iteration --------------
+ut.survival_rate_valiter(maze)
+
--- lab1/utils.py
+++ lab1/utils.py
@@ -57,31 +57,6 @@ class Minotaur:
                                 dummy = 0
 
 
-# class Player:
-#
-#     def __init__(self):
-#         self.x = 0
-#         self.y = 0
-#         self.actions = {0: (0, 0), 1: (0, -1), 2: (0, 1), 3: (-1, 0), 4: (1, 0)}
-#         self.action_names = {0: 'stay', 1: 'left', 2: 'right', 3: 'up', 4: 'down'}
-#
-#     def move(self, board, move):
-#         # Compute the future position given current (state, action)
-#
-#         self.y += self.actions[move][0]
-#         self.x += self.actions[move][1]
-#
-#         # Is the future position an impossible one ?
-#         hitting_maze_walls = (self.y == -1) or (self.y == board.shape[0]) or \
-#                              (self.x == -1) or (self.x == board.shape[1]) or \
-#                              (board[self.y, self.x] == 1)
-#
-#         # Based on the impossibilty check return the next state.
-#         if hitting_maze_walls:
-#             self.y -= self.actions[move][0]
-#             self.x -= self.actions[move][1]
-
-
 class Maze:
 
     def __init__(self):
@@ -129,7 +104,7 @@ class Maze:
         text_maze.append([None, None, None, None, None, 'B', None, None])
 
         # Create figure of the size of the maze
-        #fig = plt.figure(1, figsize=(cols, rows))
+        # fig = plt.figure(1, figsize=(cols, rows))
 
         # Remove the axis ticks and add title
         ax = plt.gca()
@@ -287,27 +262,86 @@ class Maze:
 
         return transition_probabilities
 
-    def simulate(self, start, policy):
+    def simulate(self, start, policy, method, survival_factor=None):
         path = []
 
-        # Deduce the horizon from the policy shape
-        horizon = policy.shape[1]
-        # Initialize current state and time
-        t = 0
-        s = self.map_[start]
-        # Add the starting position in the maze to the path
-        path.append(start)
-        while t < horizon:
-            # Move to next state given the policy and the current state
-            next_s = self.__move(s, policy[s, t])
-            next_s = np.random.choice(next_s)
-            # Add the position in the maze corresponding to the next state
-            # to the path
-            path.append(self.states[next_s])
-            # Update time and state for next iteration
-            t += 1
-            s = next_s
+        if method == 'DynProg':
+            # Deduce the horizon from the policy shape
+            horizon = policy.shape[1]
+            # Initialize current state and time
+            t = 0
+            s = self.map_[start]
+            # Add the starting position in the maze to the path
+            path.append(start)
+            while t < horizon:
+                # Move to next state given the policy and the current state
+                next_s = self.__move(s, policy[s, t])
+                next_s = np.random.choice(next_s)
+                # Add the position in the maze corresponding to the next state
+                # to the path
+                path.append(self.states[next_s])
+                # Update time and state for next iteration
+                t += 1
+                s = next_s
+
+        if method == 'ValIter':
+            # Initialize current state, next state and time
+            t = 0
+            s = self.map_[start]
+
+            # Loop while state is not the goal state
+            while True:
+
+                path.append(self.states[s])
+
+                # check if not dead
+                if survival_factor is not None:
+                    if np.random.random() > survival_factor:
+                        break
+
+                # Move to next state given the policy and the current state
+                if s in self.eaten_states or s in self.win_states:
+                    break
+
+                next_s = self.__move(s, policy[s])
+                # Add the position in the maze corresponding to the next state
+                # to the path
+                next_s = np.random.choice(next_s)
+
+                # Update time and state for next iteration
+                t += 1
+                s = next_s
+
         return path
+
+    def survival_rate_simulated(self, start, policy, survival_factor, num=10000):
+        won = 0
+        total_path_len = 0
+
+        for i in range(num):
+            path = self.simulate(start, policy, method="ValIter", survival_factor=survival_factor)
+            # print(path)
+            last_state = self.map_[path[-1]]
+            if last_state in self.win_states:
+                won += 1
+
+            total_path_len += len(path)
+
+        return won / num, total_path_len / num
+
+
+class AnimateGame:
+    def __init__(self, path):
+        self.maze = Maze()
+        self.path = path
+
+    def animate(self, i):
+        moves = self.path[i]
+        self.maze.board[moves[0]][moves[1]] = 2
+        self.maze.board[moves[2]][moves[3]] = -1
+        self.maze.draw()
+        self.maze.board[moves[0]][moves[1]] = 0
+        self.maze.board[moves[2]][moves[3]] = 0
 
 
 def dynamic_programming(env, horizon):
@@ -356,3 +390,83 @@ def dynamic_programming(env, horizon):
         policy[:, t] = np.argmax(Q, 1)
 
     return V, policy
+
+
+def value_iteration(env, gamma, epsilon):
+    """ Solves the shortest path problem using value iteration
+        :input Maze env           : The maze environment in which we seek to
+                                    find the shortest path.
+        :input float gamma        : The discount factor.
+        :input float epsilon      : accuracy of the value iteration procedure.
+        :return numpy.array V     : Optimal values for every state at every
+                                    time, dimension S*T
+        :return numpy.array policy: Optimal time-varying policy at every state,
+                                    dimension S*T
+    """
+    # The value itearation algorithm requires the knowledge of :
+    # - Transition probabilities
+    # - Rewards
+    # - State space
+    # - Action space
+    # - The finite horizon
+    p = env.transition_probabilities
+    r = env.rewards
+    n_states = env.n_states
+    n_actions = env.n_actions
+
+    # Required variables and temporary ones for the VI to run
+    V = np.zeros(n_states)
+    Q = np.zeros((n_states, n_actions))
+    BV = np.zeros(n_states)
+    # Iteration counter
+    n = 0
+    # Tolerance error
+    tol = (1 - gamma) * epsilon / gamma
+
+    # Initialization of the VI
+    for s in range(n_states):
+        for a in range(n_actions):
+            Q[s, a] = r[s, a] + gamma * np.dot(p[:, s, a], V)
+    BV = np.max(Q, 1)
+
+    # Iterate until convergence
+    while np.linalg.norm(V - BV) >= tol and n < 200:
+        # Increment by one the numbers of iteration
+        n += 1
+        # Update the value function
+        V = np.copy(BV)
+        # Compute the new BV
+        for s in range(n_states):
+            for a in range(n_actions):
+                Q[s, a] = r[s, a] + gamma * np.dot(p[:, s, a], V)
+        BV = np.max(Q, 1)
+        # Show error
+        # print(np.linalg.norm(V - BV))
+
+    # Compute policy
+    policy = np.argmax(Q, 1)
+    # Return the obtained policy
+    return V, policy
+
+
+def survival_rate_valiter(maze):
+    """
+
+    """
+    # Mean lifetime
+    mean_lifetime = 100
+    # Discount factor
+    gamma = 1 - (1 / mean_lifetime)
+    # Threshold
+    epsilon = 0.0001
+
+    V, policy = value_iteration(maze, gamma, epsilon)
+
+    start_A = (0, 0, 6, 5)
+
+    rate, avg_path_len = maze.survival_rate_simulated(start_A, policy, survival_factor=gamma)
+
+    print("Rate of survival = ", rate)
+    print("Average lifetime = ", avg_path_len - 1)
+
+    return rate
